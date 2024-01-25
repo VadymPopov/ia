3d3da76b915332eda1b2a565925802f2f4b722e4
VadymPopov <boycristal10@gmail.com>
2024-01-25 15:29:14 -0500

    update maps

--- src/components/EmbeddedMap/EmbeddedMap.jsx
+++ src/components/EmbeddedMap/EmbeddedMap.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const EmbeddedMap = () => {
   const apiKey = 'AIzaSyADtmfaKoDbGchjLiXWRFuxo_soqOVNqH4';
-  const lat = '43.653583460654424';
-  const lon = '-79.40671229011691';
-  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&center=${lat},${lon}&zoom=18&q=Chronic+House+Toronto`; 
+  const lat = '43.656777251560946';
+  const lon = '-79.40727752380309';
+  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&center=${lat},${lon}&zoom=18&q=Chronic+Ink+Tattoo+Shop+CollegeToronto`; 
 
   return (
     <div style={{ width: '100%', height: '500px' }}>
@@ -25,5 +25,3 @@ const EmbeddedMap = () => {
 };
 
 export default EmbeddedMap;
-
-
