fe984435127cee183ad8040559296aade4ac5f9e
VadymPopov <boycristal10@gmail.com>
2024-07-19 10:44:12 -0400

    fix: month number in checkDay

--- src/utils/checkDay.js
+++ src/utils/checkDay.js
@@ -1,9 +1,8 @@
 export const checkDay = date => {
   const ottawaDateRange = [14, 15, 16, 17, 18];
-  const day = Number(date.split('.')[1]);
-  const month = Number(date.split('.')[0]);
-  if (ottawaDateRange.includes(day) && month === 7) {
-    return '155 Loretta Ave N, Ottawa, ON K1Y 3E5';
+  const [month, day] = date.split('.').map(Number);
+  if (ottawaDateRange.includes(day) && month === 8) {
+    return '155 Loretta Ave N, Ottawa, ON K1Y 3E5, Canada';
   }
 
   return '689 St. Clair Avenue West, Toronto, Ontario M6C 1B2, Canada';
