c375211c1a86a2b10799ee9b6c678582a405c0f3
VadymPopov <boycristal10@gmail.com>
2024-11-07 11:53:49 -0500

    open nov 19

--- src/utils/isNotMonTueWed.js
+++ src/utils/isNotMonTueWed.js
@@ -4,13 +4,13 @@ export const isNotMonTueWed = date => {
   const dayNumber = new Date(date).getDate();
 
   if (
-    ((dayNumber === 5 ||
+    (dayNumber === 5 ||
       dayNumber === 6 ||
       dayNumber === 12 ||
       dayNumber === 13 ||
-      dayNumber === 18) &&
-      month === 10) ||
-    (dayNumber === 29 && month === 9)
+      dayNumber === 18 ||
+      dayNumber === 19) &&
+    month === 10
   )
     return true;
   return day !== 1 && day !== 2 && day !== 3;
